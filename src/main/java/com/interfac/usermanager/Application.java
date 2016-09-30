ab938af37c4bf86b1aa9efe802f9d583ab79a142
Ali Abdalla <ali.abdalla@sabalgroup.com>
2016-09-30 22:13:07 +0300

    can retrieve a user object from SecurityContextHolder --> modified_by works correctly now.

--- src/main/java/com/interfac/usermanager/Application.java
+++ src/main/java/com/interfac/usermanager/Application.java
@@ -33,8 +33,8 @@ public class Application {
 		repo.save(root);
 	}
 	
-	@Bean
-    AuditorAware<User> auditorProvider() {
-        return new AuditorAwareImp();
-    }
+	
+//    AuditorAware<User> auditorProvider() {
+//        return new AuditorAwareImp();
+//    }
 }
--- src/main/java/com/interfac/usermanager/config/AuditorAwareImp.java
+++ src/main/java/com/interfac/usermanager/config/AuditorAwareImp.java
@@ -2,6 +2,8 @@ package com.interfac.usermanager.config;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -9,15 +11,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import com.interfac.usermanager.user.model.User;
 import com.interfac.usermanager.user.repositories.UserRepository;
 
+@Configuration
 public class AuditorAwareImp implements AuditorAware<User> {
+	
+	@Autowired
 	private UserRepository userRepository;
 	@Override
 	public User getCurrentAuditor() {
 		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
 		List<User> users = userRepository.findUserByUserName(auth.getName());
-		System.out.println("################################################"
-				+ "###############################################");
-		System.out.println(users.get(0));
 //		if (auth == null || !auth.isAuthenticated()) {
 //		      return null;
 //		}
